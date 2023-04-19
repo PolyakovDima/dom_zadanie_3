6b326129139c889a57666b4f2ab4a07b7c6b7447
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-19 17:45:38 +0300

    cube

---
+++ task_23/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = ReadInt("N");
+
+System.Console.WriteLine(string.Join(" ", Cube(a)));
+
+int ReadInt(string argument)
+{
+    int j;
+    bool checkingOnNumber;
+    do
+    {
+        Console.Write($"Input {argument}: ");
+        checkingOnNumber = int.TryParse(Console.ReadLine(), out j);
+        
+        if (!checkingOnNumber || j <= 0)
+        {
+            Console.WriteLine("It's not a number");
+        }
+    }
+    while(!checkingOnNumber || j <= 0);
+
+    return j;
+}
+
+
+int [] Cube(int a)
+{
+    int [] C = new int [a];
+
+    for (int i = 1; i <= a; i ++)
+    {
+        C[i-1] = (int)Math.Pow(i, 3);
+    }
+
+    return C;
+}
